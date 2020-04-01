d0b7cd65e7574dda36410b8eed289a3bbc70f56e
CruzCooke <cruzvcooke@gmail.com>
2020-04-02 12:20:45 +1300

    third commit

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ import "./css/itemsdisplay.css";
 import "./css/itemdetail.css";
 import "./css/userprofile.css";
 import UserProfile from "./components/UsersProfile";
+import SignUpForm from './components/SignUpForm'
 import DisplayItems from "./components/DisplayItems";
 import Categories from "./components/Categories";
 import Home from "./components/Home";
 import ViewItems from "./components/ViewItems";
 import ItemDetail from "./components/ItemDetail"
+import Add from "./components/Add"
+import Edit from "./components/Edit"
 
 class App extends Component {
   render() {
@@ -21,8 +24,13 @@ class App extends Component {
         <Router>
           <Categories path="/categories" />
           <ViewItems path="/view-items" />
+          <DisplayItems path= "/display-items" />
+          <Add path="/add-items" />
+          <Edit path="/edit-items" />
           {/* Display items may require /display-items/:id if we use one component*/}
           {/* <DisplayItems path=""/> */}
+          <UserProfile path="/user-profile" />
+          <SignUpForm path="/signup-form" />
           <Home path="/login-signup"/>
         </Router>
         <ItemDetail />
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
+import logo from "./images/logo.png";
+import homephoto from "./images/main-illustration.png";
 
 class SignUpForm extends Component {
   render() {
-    return <React.Fragment></React.Fragment>;
+    return <React.Fragment>
+    <img src={logo} alt="logo" className="logo" />
+        <img src={homephoto} alt="home-illus" className="home-photo" />
+        
+         <div className="page"> 
+        <input type="text" placeholder="Email" className="text-input"></input>
+          <input type="text" placeholder="Password" className="text-input"></input>
+          <input type="text" placeholder="Confirm Password" className="text-input"></input>
+        <button className="btn btn-primary btn-lesswide">Sign up</button>
+</div>
+
+
+
+    </React.Fragment>;
   }
 }
 
