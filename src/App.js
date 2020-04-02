446a5034ac98f211150f622dd413e7eb7c3003d2
sammie2309 <56805412+sammie2309@users.noreply.github.com>
2020-04-03 10:43:23 +1300

    Check out added

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ import "./css/itemdetail.css";
 import "./css/userprofile.css";
 import "./css/inputs.css";
 import "./css/cart.css";
+import "./css/checkout.css";
 
 import Cart from "./components/Cart";
+import CheckOut from "./components/CheckOut";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Cart />
+        <CheckOut />
       </React.Fragment>
     );
   }
