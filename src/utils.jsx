667c305d8f70cb0366051932854e89020faca0df
frappe76 <kevin.dowd@acgedu.com>
2020-04-06 12:25:32 +1200

    form  update in edit.jsx, new path in utils.js

--- src/utils.jsx
+++ src/utils.jsx
@@ -3,6 +3,7 @@ const url = "http://localhost:4001";
 // don't add public on the end
 const images_folder = url + "/";
 const show_items = url + "/api/itemdetails";
+const update_item = url + "/api/itemdetails";
 const show_women = url + "/api/itemdetails/women";
 const show_men = url + "/api/itemdetails/men";
 
@@ -12,16 +13,16 @@ const show_men = url + "/api/itemdetails/men";
 // // POST
 // const add_item = url + "/api/itemdetails";
 
-
 // POST
 const post_comment = url + "/api/comments";
 
 export {
   url,
   show_items,
+  update_item,
   show_women,
   show_men,
-//   add_item,
+  //   add_item,
   post_comment,
-  images_folder
+  images_folder,
 };
