9897451a4beeccda7a5ce6d23d8d929093e932ba
nakitanaidu <nainai.nikita@gmail.com>
2020-04-07 22:16:54 +1200

    trying to update image

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -6,6 +6,16 @@ import Axios from "axios";
 // import NavBar from "./NavBar";
 
 export default class UserItem extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       image:''
+    }
+  }
+  
+
   usersProductDetail = (e) => {
     let temp = this.props._id;
     console.log(this.props._id);
@@ -15,7 +25,7 @@ export default class UserItem extends Component {
   render() {
 
     // how/where to display the changed image??
-    // src={UTILS.assets_url + image}
+    // src={UTILS.assets_url + item.image}
     // const image_path = UTILS.assets_url + image;
     // http://localhost:4001/assets/24c6a5f3b9cde308c1381cbb12294ace.jpg
 
@@ -23,10 +33,6 @@ export default class UserItem extends Component {
       <React.Fragment>
         <div className="item-con">
           <div className="img-con">
-            {/* 
-                  Trying to retrieve and display this data
-                  http://localhost:4001/assets/24c6a5f3b9cde308c1381cbb12294ace.jpg 
-            */}
             <img
               src={this.props.image}
               alt="item-img"
