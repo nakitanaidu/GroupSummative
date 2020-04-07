d7bd3994e71d9921d2902663771e69486ed9a0f4
CruzCooke <cruzvcooke@gmail.com>
2020-04-07 13:56:47 +1200

    fourthcommit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import logo from "./images/logo.png";
 import homephoto from "./images/main-illustration.png";
+import { navigate } from "@reach/router";
 
-
+const initialState = {
+  Username: "",
+  Password: "",
+  UsernameError:"",
+  PasswordError: "",
+}
 
 class Login extends Component {
 
-    state = {
-       Username: "",
-       Password: "",
-       UsernameError:"name is empty",
-       PasswordError: "",
 
-    };
-
+    
+    state = initialState;
+    
     handleChange = event => {
         const isCheckbox = event.target.tyoe === "checkbox";
         this.setState({
@@ -23,13 +25,40 @@ class Login extends Component {
         });
     };
 
-    
-    handleSubmit = event => {
-       event.preventDefault();
-       console.log(this.state)
-        };
-    
+    validate =() => {
+       let UsernameError="";
+       let  PasswordError= "";
 
+       if(!this.state.Username) {
+        UsernameError = "Username cannot be blank"}
+    
+    
+        if(!this.state.Password) {
+          PasswordError = "Password cannot be blank"}
+      
+       
+
+        if(UsernameError || PasswordError){
+          this.setState({UsernameError, PasswordError});
+          return false;
+        } 
+        return true;
+};
+
+
+    handleSubmit = event => {
+        const isValid = this.validate();
+       event.preventDefault();
+      if (isValid){
+         console.log(this.state) ;
+         this.setState(initialState);
+      }
+       
+        };
+     
+gotoHome = () => {
+  navigate("/")
+}
 
   render() {
     return <React.Fragment>
@@ -38,11 +67,14 @@ class Login extends Component {
 
       <div className="page">
         <form onSubmit={this.handleSubmit}>
+       
         <input type="text" placeholder="User" className="text-input" name="Username" value={this.state.Username} onChange={this.handleChange}></input>
         <div style={{fontSize: 12, color: "red"}}>{this.state.UsernameError}</div>
+       
         <input type="password" placeholder="Confirm Password" className="text-input" name="Password"  value={this.state.Password} onChange={this.handleChange}></input>
-
-        <button type="submit" className="btn btn-primary btn-lesswide">Login</button>
+        <div style={{fontSize: 12, color: "red"}}>{this.state.PasswordError}</div>
+      
+        <button type="submit" className="btn btn-primary btn-lesswide" onClick={this.gotoHome}>Login</button>
 </form>
       </div>
 
