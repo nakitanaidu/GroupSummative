2d1d6097319435d2f0e1cdc00ca91c2d114af5e6
CruzCooke <cruzvcooke@gmail.com>
2020-04-09 13:59:32 +1200

    sixth

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -31,7 +31,7 @@ class ItemDetail extends Component {
             return (
               <React.Fragment key={i}>
                 <div className="detail-img-con">
-                  <img src={item.image} alt="item-img" />
+                  <img src={`${UTILS.images_folder}` + item.image} alt="item-img" />
                 </div>
 
                 <h2 className="dark">{item.title}</h2>
