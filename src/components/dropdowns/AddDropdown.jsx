c2bfb94a04245c1eea0452ab7c4dfd37ca485337
CruzCooke <cruzvcooke@gmail.com>
2020-04-17 10:10:07 +1200

    firstcommit

--- src/components/dropdowns/AddDropdown.jsx
+++ src/components/dropdowns/AddDropdown.jsx
@@ -2,19 +2,6 @@ import React, { Component } from 'react'
 
 export default class AddDropdown extends Component {
   constructor(props) {
-<<<<<<< HEAD
-    super(props)
-
-    this.state = {
-      value: "Women's clothing",
-      name: "category",
-    }
-  }
-
-  onCategoryChanged = (e) => {
-    console.log("clicked");
-    this.setState({ name: "category" });
-=======
     super(props);
     this.state = { value: "Women's clothing" };
   }
@@ -22,7 +9,6 @@ export default class AddDropdown extends Component {
   onCategoryChanged = (e) => {
     console.log("cate click");
     this.setState({ value: e.target.value });
->>>>>>> 2eb6068853851d54a5d553720f17c95d23541657
   };
 
   render() {
@@ -33,15 +19,6 @@ export default class AddDropdown extends Component {
     // } = this.state.category;
     return (
       <React.Fragment>
-<<<<<<< HEAD
-        <select className="category-options" name={this.state.name} defaultValue={'DEFAULT'} onChange={this.onCategoryChanged}>
-          <option value="DEFAULT" disabled>Choose...</option>
-          <option className="option-style"  value="Women's clothing" >
-          Women's clothing
-          </option>
-          <option className="option-style"  value="Men's clothing"  >
-          Men's clothing
-=======
         <select
           className="category-options"
           defaultValue={this.state.value}
@@ -61,7 +38,6 @@ export default class AddDropdown extends Component {
             onClick={this.onWomenClick}
           >
             Men
->>>>>>> 2eb6068853851d54a5d553720f17c95d23541657
           </option>
         </select>
       </React.Fragment>
