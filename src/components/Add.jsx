928f17c1a6b2230c81fde3df9dfc47d21fc8a174
nakitanaidu <nainai.nikita@gmail.com>
2020-04-16 15:08:09 +1200

    paste validation for add

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -6,27 +6,136 @@ import TopNav from "./TopNav";
 import NavBar from "./NavBar";
 import AddDropdown from "./dropdowns/AddDropdown";
 
+const initialState = {
+  Title: "",
+  Price: "",
+  Size: "",
+  Condition: "",
+  Description: "",
+  TitleError: "",
+  PriceError: "",
+  SizeError: "",
+  ConditionError: "",
+  DescriptionError: "",
+}
+
+
 class Add extends Component {
   constructor(props) {
     super(props);
     this.formRef = React.createRef();
     this.state = { item_id: 0 };
+    // this.state = initialState;
   }
 
+onChangeTitle = (e) => {
+  this.setState({ Title: e.target.value });
+  const isCheckbox = e.target.type === "checkbox";
+  this.setState({
+    [e.target.name]: isCheckbox
+      ? e.target.checked
+      : e.target.value
+  });
+};
+  
+onChangePrice = (e) => {
+  this.setState({ Price: e.target.value });
+  const isCheckbox = e.target.type === "checkbox";
+  this.setState({
+    [e.target.name]: isCheckbox
+      ? e.target.checked
+      : e.target.value
+  });
+};
+  
+onChangeSize = (e) => {
+  this.setState({ Size: e.target.value });
+  const isCheckbox = e.target.type === "checkbox";
+  this.setState({
+    [e.target.name]: isCheckbox
+      ? e.target.checked
+      : e.target.value
+  });
+};
+  
+  
+onChangeCondition = (e) => {
+  this.setState({ Condition: e.target.value });
+  const isCheckbox = e.target.type === "checkbox";
+  this.setState({
+    [e.target.name]: isCheckbox
+      ? e.target.checked
+      : e.target.value
+  });
+};
+  
+onChangeDescription = (e) => {
+  this.setState({ Description: e.target.value });
+  const isCheckbox = e.target.type === "checkbox";
+  this.setState({
+    [e.target.name]: isCheckbox
+      ? e.target.checked
+      : e.target.value
+  });
+};
+  
+  // Form info validation
+  
+validate = () => {
+    
+  let TitleError = "";
+  let PriceError = "";
+  let SizeError = "";
+  let ConditionError = "";
+  let DescriptionError = "";
 
-  addProduct = (e) => {
-    e.preventDefault();
-    var formData = new FormData(this.formRef.current);
+  if (!this.state.Title) {
+    TitleError = "Invalid title";
+  }
 
-    //lets see what we have in the form
-    for (var p of formData.entries()) {
-      console.log(p);
-    }
+  if (!this.state.Price) {
+    PriceError = "invalid price";
+  }
 
-    var settings = {
-      headers: { "Content-Type": "multipart/form-data" },
-    };
+  if (!this.state.Size) {
+    SizeError = "invalid size";
+  }
 
+  if (!this.state.Condition) {
+    ConditionError = "invalid condition";
+  }
+
+  if (!this.state.Description) {
+    DescriptionError = "Invalid description";
+  }
+
+  if (TitleError || PriceError || SizeError || ConditionError || DescriptionError) {
+    this.setState({ TitleError, PriceError, SizeError, ConditionError, DescriptionError });
+    return false;
+
+  } return true;
+
+}
+  
+addProduct = (e) => {
+  e.preventDefault();
+  const isValid = this.validate();
+  if (isValid) {
+  console.log(this.state);
+  this.setState(initialState)
+
+  {var formData = new FormData(this.formRef.current);
+
+  //lets see what we have in the form
+  for (var p of formData.entries()) {
+    console.log(p);
+  }
+
+  var settings = {
+    headers: { "Content-Type": "multipart/form-data" },
+  };
+
+  
     Axios.post(UTILS.show_items, formData, settings)
       .then((res) => {
         console.log(res);
@@ -35,24 +144,27 @@ class Add extends Component {
       .catch((err) => {
         console.log(err);
       });
-  };
+  }
+}
+};
+  
+checkForURL = (s = "") => {
+  console.log("s = ", s);
+  if (s.startsWith("http")) {
+    return true;
+  }
 
-  checkForURL = (s = "") => {
-    console.log("s = ", s);
-    if (s.startsWith("http")) {
-      return true;
-    }
+  if (s.startsWith("httsp")) {
+    return true;
+  }
 
-    if (s.startsWith("httsp")) {
-      return true;
-    }
+  if (s.startsWith("//")) {
+    return true;
+  }
 
-    if (s.startsWith("//")) {
-      return true;
-    }
+  return false;
+};
 
-    return false;
-  };
 
   render() {
     return (
@@ -119,3 +231,6 @@ class Add extends Component {
 }
 
 export default Add;
+
+
+
