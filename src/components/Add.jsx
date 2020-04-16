442dc012d2429f0abe70685d4300d1b2c8a9babf
nakitanaidu <nainai.nikita@gmail.com>
2020-04-16 14:58:21 +1200

    add validation

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -4,9 +4,7 @@ import { navigate } from "@reach/router";
 import Axios from "axios";
 import TopNav from "./TopNav";
 import NavBar from "./NavBar";
-import CategorySelector from "./CategorySelector";
-import CategoryDropdown from "./CategoryDropdown"
-
+import AddDropdown from "./dropdowns/AddDropdown";
 
 const initialState = {
   Title: "",
@@ -28,180 +26,157 @@ class Add extends Component {
     this.formRef = React.createRef();
     this.state = { item_id: 0 };
     this.state = initialState;
-
   }
 
+onChangeTitle = (e) => {
+  this.setState({ Title: e.target.value });
+  const isCheckbox = e.target.type === "checkbox";
+  this.setState({
+    [e.target.name]: isCheckbox
+      ? e.target.checked
+      : e.target.value
+  });
+};
 
-  onChangeTitle = (e) => {
-    this.setState({ Title: e.target.value });
-    const isCheckbox = e.target.type === "checkbox";
-    this.setState({
-      [e.target.name]: isCheckbox
-        ? e.target.checked
-        : e.target.value
-    });
-  };
+onChangePrice = (e) => {
+  this.setState({ Price: e.target.value });
+  const isCheckbox = e.target.type === "checkbox";
+  this.setState({
+    [e.target.name]: isCheckbox
+      ? e.target.checked
+      : e.target.value
+  });
+};
 
-  onChangePrice = (e) => {
-    this.setState({ Price: e.target.value });
-    const isCheckbox = e.target.type === "checkbox";
-    this.setState({
-      [e.target.name]: isCheckbox
-        ? e.target.checked
-        : e.target.value
-    });
-  };
-
-  onChangeSize = (e) => {
-    this.setState({ Size: e.target.value });
-    const isCheckbox = e.target.type === "checkbox";
-    this.setState({
-      [e.target.name]: isCheckbox
-        ? e.target.checked
-        : e.target.value
-    });
-  };
-
-
-  onChangeCondition = (e) => {
-    this.setState({ Condition: e.target.value });
-    const isCheckbox = e.target.type === "checkbox";
-    this.setState({
-      [e.target.name]: isCheckbox
-        ? e.target.checked
-        : e.target.value
-    });
-  };
-
-  onChangeDescription = (e) => {
-    this.setState({ Description: e.target.value });
-    const isCheckbox = e.target.type === "checkbox";
-    this.setState({
-      [e.target.name]: isCheckbox
-        ? e.target.checked
-        : e.target.value
-    });
-  };
+onChangeSize = (e) => {
+  this.setState({ Size: e.target.value });
+  const isCheckbox = e.target.type === "checkbox";
+  this.setState({
+    [e.target.name]: isCheckbox
+      ? e.target.checked
+      : e.target.value
+  });
+};
 
 
-  // Form info validation
+onChangeCondition = (e) => {
+  this.setState({ Condition: e.target.value });
+  const isCheckbox = e.target.type === "checkbox";
+  this.setState({
+    [e.target.name]: isCheckbox
+      ? e.target.checked
+      : e.target.value
+  });
+};
 
-  validate = () => {
+onChangeDescription = (e) => {
+  this.setState({ Description: e.target.value });
+  const isCheckbox = e.target.type === "checkbox";
+  this.setState({
+    [e.target.name]: isCheckbox
+      ? e.target.checked
+      : e.target.value
+  });
+};
+
+// Form info validation
+
+validate = () => {
     
-   let TitleError = "";
-    let PriceError = "";
-    let SizeError = "";
-   let ConditionError = "";
-   let DescriptionError = "";
+  let TitleError = "";
+  let PriceError = "";
+  let SizeError = "";
+  let ConditionError = "";
+  let DescriptionError = "";
 
-    if (!this.state.Title) {
-      TitleError = "Invalid title";
-    }
-
-    if (!this.state.Price) {
-      PriceError = "invalid price";
-    }
-
-    if (!this.state.Size) {
-      SizeError = "invalid size";
-    }
-
-    if (!this.state.Condition) {
-      ConditionError = "invalid condition";
-    }
-
-    if (!this.state.Description) {
-      DescriptionError = "Invalid description";
-    }
-
-    if (TitleError || PriceError || SizeError || ConditionError || DescriptionError) {
-      this.setState({ TitleError, PriceError, SizeError, ConditionError, DescriptionError });
-      return false;
-
-    } return true;
-
+  if (!this.state.Title) {
+    TitleError = "Invalid title";
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  addProduct = (e) => {
-    e.preventDefault();
-    const isValid = this.validate();
-    if (isValid) {
-    console.log(this.state);
-    this.setState(initialState)
-
-    {var formData = new FormData(this.formRef.current);
-
-    //lets see what we have in the form
-    for (var p of formData.entries()) {
-      console.log(p);
-    }
-
-    var settings = {
-      headers: { "Content-Type": "multipart/form-data" },
-    };
-
-    
-      Axios.post(UTILS.show_items, formData, settings)
-        .then((res) => {
-          console.log(res);
-          navigate(`/user-product-details/${res.data._id}`);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
+  if (!this.state.Price) {
+    PriceError = "invalid price";
   }
+
+  if (!this.state.Size) {
+    SizeError = "invalid size";
+  }
+
+  if (!this.state.Condition) {
+    ConditionError = "invalid condition";
+  }
+
+  if (!this.state.Description) {
+    DescriptionError = "Invalid description";
+  }
+
+  if (TitleError || PriceError || SizeError || ConditionError || DescriptionError) {
+    this.setState({ TitleError, PriceError, SizeError, ConditionError, DescriptionError });
+    return false;
+
+  } return true;
+
+}
+
+addProduct = (e) => {
+  e.preventDefault();
+  const isValid = this.validate();
+  if (isValid) {
+  console.log(this.state);
+  this.setState(initialState)
+
+  {var formData = new FormData(this.formRef.current);
+
+  //lets see what we have in the form
+  for (var p of formData.entries()) {
+    console.log(p);
+  }
+
+  var settings = {
+    headers: { "Content-Type": "multipart/form-data" },
   };
+
+  
+    Axios.post(UTILS.show_items, formData, settings)
+      .then((res) => {
+        console.log(res);
+        navigate(`/user-product-details/${res.data._id}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+};
 
 
  
-  uploadToExpress = (e) => {
-    e.preventDefault();
-    // grab reference to the form data
-    var formData = new FormData(this.formRef.current);
-    var settings = { headers: { "Content-Type": "multipart/form-data" } };
-    console.log(">>>+ FORMDATA ", formData);
-    Axios.post(UTILS.show_items, formData, settings).then((res) => {
-      console.log(res);
-    });
-  };
+uploadToExpress = (e) => {
+  e.preventDefault();
+  // grab reference to the form data
+  var formData = new FormData(this.formRef.current);
+  var settings = { headers: { "Content-Type": "multipart/form-data" } };
+  console.log(">>>+ FORMDATA ", formData);
+  Axios.post(UTILS.show_items, formData, settings).then((res) => {
+    console.log(res);
+  });
+};
 
-  checkForURL = (s = "") => {
-    console.log("s = ", s);
-    if (s.startsWith("http")) {
-      return true;
-    }
+checkForURL = (s = "") => {
+  console.log("s = ", s);
+  if (s.startsWith("http")) {
+    return true;
+  }
 
-    if (s.startsWith("httsp")) {
-      return true;
-    }
+  if (s.startsWith("httsp")) {
+    return true;
+  }
 
-    if (s.startsWith("//")) {
-      return true;
-    }
+  if (s.startsWith("//")) {
+    return true;
+  }
 
-    return false;
-  };
+  return false;
+};
 
   render() {
     return (
@@ -215,47 +190,34 @@ class Add extends Component {
               placeholder="Title"
               name="title"
               className="text-input"
-              value={this.state.Title}
-              onChange={this.onChangeTitle}
             ></input>
-              <div style={{ fontSize: 12, color: "red" }}>{this.state.TitleError}</div>
             <input
               type="text"
               placeholder="Price"
               name="price"
               className="text-input"
-              value={this.state.Price}
-              onChange={this.onChangePrice}
             ></input>
-               <div style={{ fontSize: 12, color: "red" }}>{this.state.PriceError}</div>
             <input
               type="text"
               placeholder="Size"
               name="size"
               className="text-input"
-              value={this.state.Size}
-              onChange={this.onChangeSize}
             ></input>
-               <div style={{ fontSize: 12, color: "red" }}>{this.state.SizeError}</div>
             <input
               type="text"
               placeholder="Condition"
               name="condition"
               className="text-input"
-              value={this.state.Condition}
-              onChange={this.onChangeCondition}
             ></input>
-   <div style={{ fontSize: 12, color: "red" }}>{this.state.ConditionError}</div>
+
             <input
               type="textarea"
               placeholder="Description"
               name="description"
               className="textarea-input"
-              value={this.state.Description}
-              onChange={this.onChangeDescription}
             ></input>
 
-            <CategoryDropdown />
+            <AddDropdown />
             <input id="id" type="hidden" name="id" value={this.state.id} />
 
             <div className="uploadimg-con">
@@ -281,24 +243,3 @@ class Add extends Component {
 }
 
 export default Add;
-
-// onMenClicked = (e) => {
-//   var formData = new FormData(this.formRef.current);
-//   var mens_category = { mens_category: "men" };
-//   Axios.post(UTILS.show_items, formData, mens_category).then((res) => {
-//     console.log(res.data);
-//   });
-// };
-
-// onChangeCategory = (e) => {
-//   e.preventDefault();
-//   var formData = new FormData(this.formRef.current);
-//   var women = e.target.elements["women"].value;
-//   var men = e.target.elements["men"].value;
-//   Axios.post(UTILS.show_items, formData, {
-//     womens_category: women,
-//     mens_category: men,
-//   }).then((res) => {
-//     console.log(res);
-//   });
-// };
